9c1764dd4b5274d81db0c9ba02b1249918e2915d
Tomas Votruba <tomas.vot@gmail.com>
2015-08-02 22:07:37 +0200

    drop injection feature - rather fixtures related

--- src/Configuration/Configuration.php
+++ src/Configuration/Configuration.php
@@ -7,10 +7,7 @@
 
 namespace Zenify\DoctrineMigrations\Configuration;
 
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Migrations\Configuration\Configuration as BaseConfiguration;
-use Doctrine\DBAL\Migrations\OutputWriter;
-use Nette\DI\Container;
 use Zenify\DoctrineMigrations\DI\MigrationsExtension;
 
 
@@ -21,37 +18,6 @@ class Configuration extends BaseConfiguration
 	 * @var string
 	 */
 	private $codingStandard = MigrationsExtension::CODING_STANDARD_TABS;
-
-
-	public function __construct(Connection $connection, OutputWriter $outputWriter, Container $container)
-	{
-		parent::__construct($connection, $outputWriter);
-		$this->container = $container;
-	}
-
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getMigrationsToExecute($direction, $to)
-	{
-		$versions = parent::getMigrationsToExecute($direction, $to);
-		foreach ($versions as $version) {
-			$this->container->callInjects($version->getMigration());
-		}
-		return $versions;
-	}
-
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getVersion($version)
-	{
-		$version = parent::getVersion($version);
-		$this->container->callInjects($version->getMigration());
-		return $version;
-	}
 
 
 	/**
