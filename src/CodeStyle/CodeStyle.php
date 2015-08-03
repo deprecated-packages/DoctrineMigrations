4aed4ddfe8baf9ab4d77075f12ef1b00eca03b70
Tomas Votruba <tomas.vot@gmail.com>
2015-08-03 05:29:43 +0200

    CodeStyle added

---
+++ src/CodeStyle/CodeStyle.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of Zenify
+ * Copyright (c) 2014 Tomas Votruba (http://tomasvotruba.cz)
+ */
+
+namespace Zenify\DoctrineMigrations\CodeStyle;
+
+
+class CodeStyle
+{
+
+	/**
+	 * @param string $file
+	 */
+	public static function convertSpacesToTabsForFile($file)
+	{
+		$code = file_get_contents($file);
+		$code = preg_replace("/ {4}/", "\t", $code);
+		file_put_contents($file, $code);
+	}
+
+}
---
+++ tests/CodeStyle/CodeStyleTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of DoctrineMigrations
+ *
+ * Copyright (c) 2014 Pears Health Cyber, s.r.o. (http://pearshealthcyber.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file LICENSE that was distributed with this source code.
+ */
+
+
+
+class CodeStyleTest
+{
+
+}
