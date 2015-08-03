9d1046157ab8199147f96e31492984f17eb31c8e
Tomas Votruba <tomas.vot@gmail.com>
2015-08-03 13:01:25 +0200

    tests: CodeStyle - for spaces

--- tests/CodeStyle/CodeStyleTest.php
+++ tests/CodeStyle/CodeStyleTest.php
@@ -19,4 +19,15 @@ class CodeStyleTest extends PHPUnit_Framework_TestCase
 		$this->assertStringEqualsFile($file, "\thi");
 	}
 
+
+	public function testKeepSpaces()
+	{
+		$file = TEMP_DIR . '/some-spaced-text-file.txt';
+		file_put_contents($file, '    hi');
+		(new CodeStyle(CodeStyle::INDENTATION_SPACES))->applyForFile($file);
+
+		$this->assertStringEqualsFile($file, '    hi');
+		$this->assertStringNotEqualsFile($file, "\thi");
+	}
+
 }
