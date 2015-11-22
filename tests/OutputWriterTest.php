bc3c0aad99330be90ae2a29d5916753bc1a820b1
Tomas Votruba <tomas.vot@gmail.com>
2015-11-23 00:45:28 +0100

    remove unused namespace

--- tests/OutputWriterTest.php
+++ tests/OutputWriterTest.php
@@ -4,10 +4,7 @@ namespace Zenify\DoctrineMigrations\Tests;
 
 use PHPUnit_Framework_Assert;
 use PHPUnit_Framework_TestCase;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Output\NullOutput;
 use Zenify\DoctrineMigrations\OutputWriter;
 
 
